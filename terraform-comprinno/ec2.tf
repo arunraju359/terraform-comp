ae02fc2d926fbe03b30aa48d6e46b23b6703c64d
arunraju359 <arunraju359@gmail.com>
2022-08-17 13:15:04 +0530

    commit

--- terraform-comprinno/ec2.tf
+++ terraform-comprinno/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
   vpc_id            = aws_vpc.main.id
   cidr_block        = element (var.PRIVATE_CIDR, 0)
