caee6b4e96a0f258964176af39cde4bb96b378fc
arunraju359 <arunraju359@gmail.com>
2022-08-17 12:58:13 +0530

    commit

--- terraform-comprinno/route.tf
+++ terraform-comprinno/route.tf
@@ -25,12 +25,12 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "public" {
   count = length(var.PUBLIC_CIDR)
-  subnet_id      = aws_subnet.public.id[count.index]
+  subnet_id      = aws_subnet.public.[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.PRIVATE_CIDR)
-  subnet_id      = aws_subnet.private.id[count.index]
+  subnet_id      = aws_subnet.private.[count.index].id
   route_table_id = aws_route_table.private.id
 }
