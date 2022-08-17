145c1a3fb4da807c6c3ae5e513faa2c2e4da4795
arunraju359 <arunraju359@gmail.com>
2022-08-17 12:47:25 +0530

    commit

--- terraform-comprinno/route.tf
+++ terraform-comprinno/route.tf
@@ -22,3 +22,13 @@ resource "aws_route_table" "private" {
     Name = "private-route"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.*.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.*.id
+  gateway_id = aws_nat_gateway.ngw.id
+}
--- terraform-comprinno/vars.tf
+++ terraform-comprinno/vars.tf
@@ -1,3 +1,4 @@
 variable "VPC_CIDR" {}
 variable "PUBLIC_CIDR" {}
 variable "PRIVATE_CIDR" {}
+variable "SUBNET_ID"  {}
