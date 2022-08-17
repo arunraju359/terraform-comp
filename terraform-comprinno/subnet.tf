69525d2a98abe9db17dfeac86f461b236d37093c
arunraju359 <arunraju359@gmail.com>
2022-08-17 12:17:57 +0530

    commit

---
+++ terraform-comprinno/subnet.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  count = length(var.PUBLIC_CIDR)
+
+  cidr_block = element (var.PUBLIC_CIDR, count.index)
+
+  tags = {
+    Name = "Public-Subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+count = length(var.PRIVATE_CIDR)
+
+cidr_block = element (var.PRIVATE_CIDR, count.index)
+
+  tags = {
+    Name = "Private-Subnet-${count.index + 1}"
+  }
+}
