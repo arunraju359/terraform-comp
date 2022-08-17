5bb8edc43c5c5a68f2480c4041be63b0de907f54
arunraju359 <arunraju359@gmail.com>
2022-08-17 12:48:44 +0530

    commit

--- terraform-comprinno/route.tf
+++ terraform-comprinno/route.tf
@@ -31,4 +31,5 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.*.id
   gateway_id = aws_nat_gateway.ngw.id
+  route_table_id = aws_route_table.private.id
 }
